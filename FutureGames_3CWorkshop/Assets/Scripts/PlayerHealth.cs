d4fd52bd90531f776668c25ad51869e5823a320a
filur1n <william.chung@edu.futuregames.nu>
2022-10-07 17:35:35 +0200

    Different Enemy that walks faster

--- FutureGames_3CWorkshop/Assets/Scripts/PlayerHealth.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/PlayerHealth.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth;
-    public int currentHealth;
+    private int currentHealth;
 
+    public float flashLength;
+    private float flashCounter;
+
+    private Renderer rend;
+    private Color storedColor;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = startingHealth;
+        rend = GetComponent<Renderer>();
+        storedColor = rend.material.GetColor("_Color");
     }
 
     // Update is called once per frame
@@ -20,10 +29,21 @@ public class PlayerHealth : MonoBehaviour
         if (currentHealth <=0)
         {
             gameObject.SetActive(false);
+            SceneManager.LoadScene("Level");
+        }
+        if(flashCounter > 0)
+        {
+            flashCounter -= Time.deltaTime;
+            if(flashCounter <= 0)
+            {
+                rend.material.SetColor("_Color", storedColor);
+            }
         }
     }
     public void HurtPlayer(int damageAmount)
     {
         currentHealth -= damageAmount;
+        flashCounter = flashLength;
+        rend.material.SetColor("_Color", Color.white);
     }
 }
