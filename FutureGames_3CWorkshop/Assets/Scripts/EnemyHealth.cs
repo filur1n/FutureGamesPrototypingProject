73d617c6c3dffb22fd3b7e1d0a97ce40db006951
filur1n <william.chung@edu.futuregames.nu>
2022-10-08 12:58:34 +0200

    Starting over when you kill all enemies

--- FutureGames_3CWorkshop/Assets/Scripts/EnemyHealth.cs
+++ FutureGames_3CWorkshop/Assets/Scripts/EnemyHealth.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int health;
     private int currentHealth;
 
+    public int deathCount;
+
 
 
     void Start()
     {
         currentHealth = health;
+        deathCount = 0;
     }
 
 
@@ -21,6 +25,15 @@ public class EnemyHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
+            deathCount++;
+
+          
+        }
+
+        if (deathCount >= 3)
+        {
+            SceneManager.LoadScene("Level");
+            Debug.LogError("Should change the scene");
         }
     }
 
